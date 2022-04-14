a64cdcb03c2beb27280784ff1392bc79ce32c21a
MikeSherif <100560657+MikeSherif@users.noreply.github.com>
2022-04-14 23:19:21 +0700

    аыва

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -3,8 +3,10 @@ $(document).ready(function (){
         $(this).fadeOut(500,function (){
             $(this).attr("src","img/lol.jpg").fadeIn(500);
         });
-
     });
-
 });
 
+$('a[href^="#"]').click(function (){
+    let valHref = $(this).attr("href");
+    $('html,body').animate({scrollTop : $(valHref).offset().top -35 + "px"});
+});
